30c01fe0307956f90cf720472f4e4d6ca34fd616
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-05-02 21:58:34 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	argsWithoutProg := os.Args[1:]
+	result1 := strings.Join(argsWithoutProg, " ")
+	fmt.Print("Hello ")
+
+	if result1 != "" {
+		fmt.Print(result1 + " ")
+	}
+
+	fmt.Println("welcome to the world of Go!")
+	fmt.Println("                                                                           ")
+	fmt.Println("                            Nmdhysoo++++oosyhdN                            ")
+	fmt.Println("                    NmN  dy+/++/:::::::://////++sdNhyhd                    ")
+	fmt.Println("                  y+//+s++:-.``.-//::://-`    `://+os+/+m                  ")
+	fmt.Println("                 y:sNd+:+...      :/::/./:      :/:/yh::o                  ")
+	fmt.Println("                 h//h/::/+Nho      o:/-y h/     `o:::s/+m                  ")
+	fmt.Println("                  myo:::+-so-     -+::+-/:`    `//:::/m                    ")
+	fmt.Println("                   m:::://-.` `../+hmmms:-...-://:::::h                    ")
+	fmt.Println("                   h::::::/:///:/+/yhhy//+////::::::::o                    ")
+	fmt.Println("                   y:::::::::::://:--:-:-+::::::::::::/                    ")
+	fmt.Println("                   y:::::::::::::/+`-:`+//:::::::::::::                    ")
+	fmt.Println("                   h::::::::::::::+:/+-+:::::::::::::::N                   ")
+	fmt.Println("                   d::::::::::::::::::/::::::::::::::::N                   ")
+	fmt.Println("                   N:::::::::::::::::::::::::::::::::::N                   ")
+	fmt.Println("                    /::::::::::::::::::::::::::::::::::m                   ")
+	fmt.Println("                  NN+::::::::::::::::::::::::::::::::::dmN                 ")
+	fmt.Println("                h/:/+::::::::::::::::::::::::::::::::::o-:y                ")
+	fmt.Println("                h+sd/::::::::::::::::::::::::::::::::::yyod                ")
+	fmt.Println("                   N/::::::::::::::::::::::::::::::::::y                   ")
+	fmt.Println("                   m:::::::::::::::::::::::::::::::::::s                   ")
+	fmt.Println("                   d:::::::::::::::::::::::::::::::::::o                   ")
+	fmt.Println("                   h:::::::::::::::::::::::::::::::::::s                   ")
+	fmt.Println("                   h:::::::::::::::::::::::::::::::::::y                   ")
+	fmt.Println("                   d:::::::::::::::::::::::::::::::::::d                   ")
+	fmt.Println("                   N::::::::::::::::::::::::::::::::::+                    ")
+	fmt.Println("                    o:::::::::::::::::::::::::::::::::d                    ")
+	fmt.Println("                    N+::::::::::::::::::::::::::::::/y                     ")
+	fmt.Println("                     Ns/:::::::::::::::::::::::::::/h                      ")
+	fmt.Println("                      No+//::::::::::::::::::::://+om                      ")
+	fmt.Println("                     s:.-+yyo////::::::::////+shms-./h                     ")
+	fmt.Println("                     y/+h     NmddhhhhhhddmN      moym                     ")
+	fmt.Println("                                                                           ")
+
+}
